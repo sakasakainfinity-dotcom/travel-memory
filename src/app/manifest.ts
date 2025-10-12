bc6f1877c5f3b7dc58aa6c5cbc7ec737ff6a6dcb
sakasakainfinity-dotcom <sakasaka_infinity@icloud.com>
2025-10-12 13:59:52 +0900

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
 　　　  { src: "/icons/icon-192-v2.png", sizes: "192x192", type: "image/png" },
 　　　  { src: "/icons/icon-512-v2.png", sizes: "512x512", type: "image/png" },
  　　　 { src: "/icons/maskable-512-v2.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
-]
+　]
     ],
   };
 }
