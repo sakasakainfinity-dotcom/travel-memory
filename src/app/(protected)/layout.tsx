5f7c036a26d51bd8a50ac7924af169621b9233ea
sakasakainfinity-dotcom <sakasaka_infinity@icloud.com>
2026-02-05 11:46:14 +0900

    Update layout.tsx

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -5,7 +5,7 @@ export default function ProtectedLayout({ children }: { children: React.ReactNod
   return (
     <AuthGate>
       <header style={{ padding: 12, display: "flex", justifyContent: "space-between" }}>
-        <div>Travel Memory</div>
+        <div>PhotoMapper</div>
       </header>
       {children}
     </AuthGate>
